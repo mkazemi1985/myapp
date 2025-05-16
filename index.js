a7638e3136dddf4a3d600ce2cb8537fc046df028
Mohammad Kazemi <de.m.kazemi@gmail.com>
2025-05-16 13:49:31 +0330

    Initial commit - minimal Node.js app with systemd setup

---
+++ index.js
@@ -0,0 +1,12 @@
+const http = require('http');
+
+const PORT = 3000;
+
+const server = http.createServer((req, res) => {
+  res.end('✅ Server is running!');
+});
+
+server.listen(PORT, () => {
+  console.log(`🚀 Server running at http://localhost:${PORT}`);
+});
+
